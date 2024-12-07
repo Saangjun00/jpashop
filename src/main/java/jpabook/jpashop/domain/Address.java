abed6ebf763f9b913ffd2be3f85735cef3551fad
Saangjun00 <149948808+Saangjun00@users.noreply.github.com>
2024-12-07 16:00:27 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    // 변경 불가능하게 생성해야 함
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
